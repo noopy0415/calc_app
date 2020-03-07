4b964f910f6735120cfb8944fbc603bb7de015e0
noopy0415 <noopy0415@gmail.com>
2020-03-07 17:41:54 +0900

    エラー出ちゃう！

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get 'calcs/:val1/:calc/:val2' => 'calcs#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
